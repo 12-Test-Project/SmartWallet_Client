341bbbe5affe9ab4fc0ad556d4ebe96507861582
AdryDev <adrydevmateo@gmail.com>
2025-03-17 21:43:26 -0400

    new route added

--- src/components/header.component.tsx
+++ src/components/header.component.tsx
@@ -1,23 +1,23 @@
 import { useEffect, useState } from 'react'
 import {
-  Dialog,
-  DialogPanel,
-  // Disclosure,
-  // DisclosureButton,
-  // DisclosurePanel,
-  // Popover,
-  // PopoverButton,
-  PopoverGroup,
-  // PopoverPanel,
+   Dialog,
+   DialogPanel,
+   // Disclosure,
+   // DisclosureButton,
+   // DisclosurePanel,
+   // Popover,
+   // PopoverButton,
+   PopoverGroup,
+   // PopoverPanel,
 } from '@headlessui/react'
 import {
-  // ArrowPathIcon,
-  Bars3Icon,
-  // ChartPieIcon,
-  // CursorArrowRaysIcon,
-  // FingerPrintIcon,
-  // SquaresPlusIcon,
-  XMarkIcon,
+   // ArrowPathIcon,
+   Bars3Icon,
+   // ChartPieIcon,
+   // CursorArrowRaysIcon,
+   // FingerPrintIcon,
+   // SquaresPlusIcon,
+   XMarkIcon,
 } from '@heroicons/react/24/outline'
 // import { 
 //   ChevronDownIcon, 
@@ -38,41 +38,63 @@ import LOGO from "@assets/logo.svg"
 // ]
 
 export default function Header() {
-  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
-  const currentLocation = useLocation()
-  const [hideElement, setHideElement] = useState(false)
+   const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+   const currentLocation = useLocation()
+   const [hideElement, setHideElement] = useState(false)
 
-  useEffect(() => {
-    setHideElement(currentLocation.pathname.includes('transactions') ? true : false)
-  }, [currentLocation])
+   const routeList: Array<{
+      to: string,
+      label: string,
+   }> = [
+         {
+            to: "/",
+            label: "Inicio"
+         },
+         {
+            to: "/transactions",
+            label: "Transacciones"
+         },
+         {
+            to: "/salary",
+            label: "Salario"
+         },
+         {
+            to: "/about",
+            label: "Acerca De"
+         }
+      ]
 
-  const logo = LOGO
+   useEffect(() => {
+      setHideElement(currentLocation.pathname.includes('transactions') ? true : false)
+   }, [currentLocation])
 
-  return (
-    <header className="bg-white bg-clip-border">
-      <nav aria-label="Global" className="mx-auto flex max-w-[1600px] items-center justify-between p-6 lg:px-8">
-        <div className="flex lg:flex-1">
-          <Link to="/" className="[&.active]:font-bold -m-1.5 p-1.5">
-            <span className="sr-only">Smartwallet</span>
-            <img
-              alt="logo"
-              src={logo}
-              className="h-8 w-auto"
-            />
-          </Link>
-        </div>
-        <div className="flex lg:hidden">
-          <button
-            type="button"
-            onClick={() => setMobileMenuOpen(true)}
-            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
-          >
-            <span className="sr-only">Open main menu</span>
-            <Bars3Icon aria-hidden="true" className="size-6" />
-          </button>
-        </div>
-        <PopoverGroup className="hidden lg:flex lg:gap-x-12">
-          {/* <Popover className="relative">
+   const logo = LOGO
+
+   return (
+      <header className="bg-white bg-clip-border">
+         <nav aria-label="Global" className="mx-auto flex max-w-[1600px] items-center justify-between p-6 lg:px-8">
+            <div className="flex lg:flex-1">
+               <Link to="/" className="[&.active]:font-bold -m-1.5 p-1.5">
+                  <span className="sr-only">Smartwallet</span>
+                  <img
+                     alt="logo"
+                     src={logo}
+                     className="h-8 w-auto"
+                  />
+               </Link>
+            </div>
+            <div className="flex lg:hidden">
+               <button
+                  type="button"
+                  onClick={() => setMobileMenuOpen(true)}
+                  className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+               >
+                  <span className="sr-only">Open main menu</span>
+                  <Bars3Icon aria-hidden="true" className="size-6" />
+               </button>
+            </div>
+            <PopoverGroup className="hidden lg:flex lg:gap-x-12">
+               {/* <Popover className="relative">
             <PopoverButton className="flex items-center gap-x-1 text-sm/6 font-semibold text-gray-900">
               Product
               <ChevronDownIcon aria-hidden="true" className="size-5 flex-none text-gray-400" />
@@ -112,47 +134,52 @@ export default function Header() {
             </PopoverPanel>
           </Popover> */}
 
-          <Link to="/" className="[&.active]:font-bold text-sm/6 font-semibold text-gray-900">
-            Inicio
-          </Link>
-          <Link to="/transactions" className="[&.active]:font-bold text-sm/6 font-semibold text-gray-900">
-            Transacciones
-          </Link>
-          <Link to="/about" className="[&.active]:font-bold text-sm/6 font-semibold text-gray-900">
-            Acerca De
-          </Link>
-        </PopoverGroup>
-        <div hidden={hideElement} className="hidden lg:flex lg:flex-1 lg:justify-end">
-          <Link to="/signin" className="[&.active]:font-bold text-sm/6 font-semibold text-gray-900">
-            Acceder <span aria-hidden="true">&rarr;</span>
-          </Link>
-        </div>
-      </nav>
-      <Dialog open={mobileMenuOpen} onClose={setMobileMenuOpen} className="lg:hidden">
-        <div className="fixed inset-0 z-10" />
-        <DialogPanel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
-          <div className="flex items-center justify-between">
-            <Link to="/" className="[&.active]:font-bold -m-1.5 p-1.5">
-              <span className="sr-only">Smartwallet</span>
-              <img
-                alt="logo"
-                src={logo}
-                className="h-8 w-auto"
-              />
-            </Link>
-            <button
-              type="button"
-              onClick={() => setMobileMenuOpen(false)}
-              className="-m-2.5 rounded-md p-2.5 text-gray-700"
-            >
-              <span className="sr-only">Close menu</span>
-              <XMarkIcon aria-hidden="true" className="size-6" />
-            </button>
-          </div>
-          <div className="mt-6 flow-root">
-            <div className="-my-6 divide-y divide-gray-500/10">
-              <div className="space-y-2 py-6">
-                {/* <Disclosure as="div" className="-mx-3">
+               {/* <Link to="/" className="[&.active]:font-bold text-sm/6 font-semibold text-gray-900">
+                  Inicio
+               </Link>
+               <Link to="/transactions" className="[&.active]:font-bold text-sm/6 font-semibold text-gray-900">
+                  Transacciones
+               </Link>
+               <Link to="/about" className="[&.active]:font-bold text-sm/6 font-semibold text-gray-900">
+                  Acerca De
+               </Link> */}
+               {routeList.map((route) => (
+                  <Link key={route.to} to={route.to} className="[&.active]:font-bold -mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">
+                     {route.label}
+                  </Link>
+               ))}
+            </PopoverGroup>
+            <div hidden={hideElement} className="hidden lg:flex lg:flex-1 lg:justify-end">
+               <Link to="/signin" className="[&.active]:font-bold text-sm/6 font-semibold text-gray-900">
+                  Acceder <span aria-hidden="true">&rarr;</span>
+               </Link>
+            </div>
+         </nav>
+         <Dialog open={mobileMenuOpen} onClose={setMobileMenuOpen} className="lg:hidden">
+            <div className="fixed inset-0 z-10" />
+            <DialogPanel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+               <div className="flex items-center justify-between">
+                  <Link to="/" className="[&.active]:font-bold -m-1.5 p-1.5">
+                     <span className="sr-only">Smartwallet</span>
+                     <img
+                        alt="logo"
+                        src={logo}
+                        className="h-8 w-auto"
+                     />
+                  </Link>
+                  <button
+                     type="button"
+                     onClick={() => setMobileMenuOpen(false)}
+                     className="-m-2.5 rounded-md p-2.5 text-gray-700"
+                  >
+                     <span className="sr-only">Close menu</span>
+                     <XMarkIcon aria-hidden="true" className="size-6" />
+                  </button>
+               </div>
+               <div className="mt-6 flow-root">
+                  <div className="-my-6 divide-y divide-gray-500/10">
+                     <div className="space-y-2 py-6">
+                        {/* <Disclosure as="div" className="-mx-3">
                   <DisclosureButton className="group flex w-full items-center justify-between rounded-lg py-2 pr-3.5 pl-3 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">
                     Product
                     <ChevronDownIcon aria-hidden="true" className="size-5 flex-none group-data-open:rotate-180" />
@@ -170,7 +197,7 @@ export default function Header() {
                     ))}
                   </DisclosurePanel>
                 </Disclosure> */}
-                <Link to="/" className="[&.active]:font-bold -mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">
+                        {/* <Link to="/" className="[&.active]:font-bold -mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">
                   Inicio
                 </Link>
                 <Link to="/transactions" className="[&.active]:font-bold -mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">
@@ -178,17 +205,22 @@ export default function Header() {
                 </Link>
                 <Link to="/about" className="[&.active]:font-bold -mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">
                   Acerca De
-                </Link>
-              </div>
-              <div hidden={hideElement} className="py-6">
-                <Link to="/signin" className="[&.active]:font-bold -mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">
-                  Acceder
-                </Link>
-              </div>
-            </div>
-          </div>
-        </DialogPanel>
-      </Dialog>
-    </header>
-  )
+                </Link> */}
+                        {routeList.map((route) => (
+                           <Link key={route.to} to={route.to} className="[&.active]:font-bold -mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">
+                              {route.label}
+                           </Link>
+                        ))}
+                     </div>
+                     <div hidden={hideElement} className="py-6">
+                        <Link to="/signin" className="[&.active]:font-bold -mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">
+                           Acceder
+                        </Link>
+                     </div>
+                  </div>
+               </div>
+            </DialogPanel>
+         </Dialog>
+      </header>
+   )
 }
--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,9 +1,9 @@
-import { homeRoute, aboutRoute, signUpRoute } from ".";
+import { homeRoute, aboutRoute, signUpRoute, adviceRoute, salaryRoute } from ".";
 import {
-  createRootRoute,
-  createRouter,
-  Outlet,
-  useLocation,
+   createRootRoute,
+   createRouter,
+   Outlet,
+   useLocation,
 } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 import { signInRoute } from "./signIn.route";
@@ -12,32 +12,32 @@ import { Header } from "@/components";
 import Footer from "@/components/footer.component";
 
 export const rootRoute = createRootRoute({
-  component: () => {
-    const currentLocation = useLocation()
+   component: () => {
+      const currentLocation = useLocation()
 
-    return (
-      <>
-        <Header />
-        <div className="mx-auto max-w-[1600px] p-6 lg:px-8 mb-20">
-          <Outlet />
-        </div>
-        {
-          currentLocation.pathname.includes('transactions') ? null : (
-            <Footer />
-          )
-        }
-        <TanStackRouterDevtools />
-      </>
-    )
-  },
+      return (
+         <>
+            <Header />
+            <div className="mx-auto max-w-[1600px] p-6 lg:px-8 mb-20">
+               <Outlet />
+            </div>
+            {
+               currentLocation.pathname.includes('transactions') ? null : (
+                  <Footer />
+               )
+            }
+            <TanStackRouterDevtools />
+         </>
+      )
+   },
 });
 
-const routeTree = rootRoute.addChildren([homeRoute, aboutRoute, signUpRoute, signInRoute, transactionsRoute]);
+const routeTree = rootRoute.addChildren([homeRoute, aboutRoute, signUpRoute, signInRoute, transactionsRoute, adviceRoute, salaryRoute]);
 
 export const router = createRouter({ routeTree });
 
 declare module "@tanstack/react-router" {
-  interface Register {
-    router: typeof router;
-  }
+   interface Register {
+      router: typeof router;
+   }
 }
